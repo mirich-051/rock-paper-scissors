59d71007b8c4da1139b8e9f68be089f36acf4b52
mirich-051 <crobird10@gmail.com>
2026-09-18 16:34:10 +0200

    Add index.html, rps.js files

---
+++ rps.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('button');
+
+    function btnPlay() {
+      let letter = prompt("Choose between r, p, or s. Please enter one letter: ");
+
+}
+    btn.addEventListener('click', btnPlay);

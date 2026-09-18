b18a30e662a6c946bd6fbfc9f01538d06bca89e5
mirich-051 <crobird10@gmail.com>
2026-09-18 17:00:48 +0200

    Add function getComputerChoice and tested with console.log()

--- rps.js
+++ rps.js
@@ -1,7 +1,24 @@
-const btn = document.querySelector('button');
 
-    function btnPlay() {
-      let letter = prompt("Choose between r, p, or s. Please enter one letter: ");
+/* const btn = document.querySelector('button');
+
+function btnPlay() {
+  let letter = prompt("Choose between r, p, or s. Please enter one letter: ");
 
 }
-    btn.addEventListener('click', btnPlay);
+btn.addEventListener('click', btnPlay); */
+
+function getComputerChoice() {
+  let num = Math.floor(Math.random() * 3) + 1;
+  
+  if (num === 1) {
+    return "rock";
+  } else if (num === 2) {
+    return "paper";
+  } else if (num === 3) {
+    return "scissors";
+  }
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(getComputerChoice());
+}
